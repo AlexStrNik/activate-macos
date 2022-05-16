c289f3b696afce8304563fe37119ef99d8645c98
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2022-05-16 20:13:23 +0300

    feat: initial commit

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# activate-macos
+
+![Example](./screenshot.png)
+
+The "Activate Windows" watermark ported to MacOS with Electron
+
+Inspired by https://github.com/MrGlockenspiel/activate-linux
+
+## Building
+
+```bash
+> npm i
+```
+
+and then
+
+```
+> npx electron-builder build --macos
+```
+
+> App `.dmg` is located in `dist/` folder
---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+
+app.whenReady().then(() => {
+  const { screen } = require("electron");
+
+  app.dock.hide();
+  const win = new BrowserWindow({
+    width: 340,
+    height: 120,
+    transparent: true,
+    frame: false,
+    resizable: false,
+    alwaysOnTop: true,
+    minimizable: false,
+    maximizable: false,
+  });
+  win.setAlwaysOnTop(true, "floating");
+  win.setVisibleOnAllWorkspaces(true, {
+    visibleOnFullScreen: true,
+    skipTransformProcessType: false,
+  });
+  win.setFullScreenable(false);
+
+  win.setIgnoreMouseEvents(true);
+  win.setPosition(
+    screen.getPrimaryDisplay().workAreaSize.width - 340,
+    screen.getPrimaryDisplay().workAreaSize.height
+  );
+
+  win.loadFile("index.html");
+});
